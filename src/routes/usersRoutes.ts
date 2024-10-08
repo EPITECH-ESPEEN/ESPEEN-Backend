7565956d8880a8ec13a57b8bb3285fc527825f81
Tech0ne <clement.piasco@epitech.eu>
2024-10-08 23:17:04 +0200

    [+] Added all (?) CRUD for Users

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { getUsers } from "../controllers/userController";
-import { isAuthenticatedUser  } from "../middlewares/userAuthentication";
+import { getUsers, getUser, updateUser, deleteUser } from "../controllers/userController";
+import { isAuthenticatedUser } from "../middlewares/userAuthentication";
 
 const router = express.Router();
 
 router.route("/users").get(isAuthenticatedUser, getUsers);
+router.route("/users/:id").get(isAuthenticatedUser, getUser);
+router.route("/users/:id").put(isAuthenticatedUser, updateUser);
+router.route("/users/:id").delete(isAuthenticatedUser, deleteUser);
 
 export default router;
