08d14357bf3028c81815336fee23a288d8a5fa51
Dan13615 <daniil.stepanov@epitech.eu>
2024-10-10 23:08:05 +0200

    [+] services controller

--- src/routes/serviceRoutes.ts
+++ src/routes/serviceRoutes.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import { getCurrentWeather } from "../services/weatherServices";
 import { isAuthenticatedUser } from "../middlewares/userAuthentication";
+import { getAllServices, getServiceById, createService, updateService, deleteService } from "../controllers/serviceController";
 
 const router = express.Router();
+
+router.route("/services").get(isAuthenticatedUser, getAllServices);
+router.route("/services/:id").get(isAuthenticatedUser, getServiceById);
+router.route("/services").post(isAuthenticatedUser, createService);
+router.route("/services/:id").put(isAuthenticatedUser, updateService);
+router.route("/services/:id").delete(isAuthenticatedUser, deleteService);
 
 router.route("/weather/:city").get(isAuthenticatedUser, getCurrentWeather);
 
